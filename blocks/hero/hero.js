852cb876d12c541f660c53b893c0d98cd8e4c1e9
AnastasiiaDesiateryk <anastasiiadesiateryk@gmail.com>
2025-05-15 22:43:56 +0300

    Update hero.js

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -2,13 +2,16 @@ export default function decorate(block) {
   block.innerHTML = `
     <picture>
       <source 
-        srcset="https://images.unsplash.com/photo-1503264116251-35a269479413?w=600&auto=format&fit=crop 600w,
-                https://images.unsplash.com/photo-1503264116251-35a269479413?w=1200&auto=format&fit=crop 1200w"
-        sizes="(max-width: 600px) 600px, 1200px"
+        srcset="https://images.unsplash.com/photo-1503264116251-35a269479413?w=600&auto=format&fit=crop"
+        media="(max-width: 600px)"
+        type="image/jpeg">
+      <source 
+        srcset="https://images.unsplash.com/photo-1503264116251-35a269479413?w=1200&auto=format&fit=crop"
+        media="(min-width: 601px)"
         type="image/jpeg">
       <img
         src="https://images.unsplash.com/photo-1503264116251-35a269479413?w=1200&auto=format&fit=crop"
-        alt="Abstractbackground"
+        alt="Abstract background"
         width="1200"
         height="600"
         loading="eager"
