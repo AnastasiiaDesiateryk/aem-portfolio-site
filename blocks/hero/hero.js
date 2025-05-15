0ff38b08f759abc627065e7ebba6af83ac033a43
AnastasiiaDesiateryk <anastasiiadesiateryk@gmail.com>
2025-05-16 00:08:51 +0300

    Update hero.js

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,13 +1,1 @@
-export default function decorate(block) {
-  block.innerHTML = `
-    <img
-      src="https://images.unsplash.com/photo-1503264116251-35a269479413?w=1200&auto=format&fit=crop"
-      alt="Abstract background"
-      class="hero-image"
-      width="1200"
-      height="600"
-      loading="eager"
-      decoding="async"
-    />
-  ;
-}
+
