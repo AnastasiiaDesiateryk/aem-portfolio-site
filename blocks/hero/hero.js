ace8c8b327752be34f493714b7e3586027056970
AnastasiiaDesiateryk <anastasiiadesiateryk@gmail.com>
2025-05-16 00:03:49 +0300

    Update hero.js

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -2,9 +2,11 @@ export default function decorate(block) {
   block.innerHTML = `
     <picture>
       <source 
-        srcset="https://images.unsplash.com/photo-1503264116251-35a269479413?w=600&auto=format&fit=crop 600w,
-                https://images.unsplash.com/photo-1503264116251-35a269479413?w=1200&auto=format&fit=crop 1200w"
-        sizes="(max-width: 600px) 600px, 1200px"
+        srcset="
+          https://images.unsplash.com/photo-1503264116251-35a269479413?w=480&auto=format&fit=crop 480w,
+          https://images.unsplash.com/photo-1503264116251-35a269479413?w=800&auto=format&fit=crop 800w,
+          https://images.unsplash.com/photo-1503264116251-35a269479413?w=1200&auto=format&fit=crop 1200w"
+        sizes="(max-width: 480px) 100vw, (max-width: 800px) 100vw, 1200px"
         type="image/jpeg">
       <img
         src="https://images.unsplash.com/photo-1503264116251-35a269479413?w=1200&auto=format&fit=crop"
@@ -14,5 +16,7 @@ export default function decorate(block) {
         loading="eager"
         decoding="async"
       />
-  
+    </picture>
+  `;
 }
+
