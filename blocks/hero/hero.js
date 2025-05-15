b08ec1c1cd7b84ba4d8825258a36187c958d9a3c
AnastasiiaDesiateryk <anastasiiadesiateryk@gmail.com>
2025-05-16 00:56:33 +0300

    Update hero.js

--- blocks/hero/hero.js
+++ blocks/hero/hero.js
@@ -1,32 +1,26 @@
+import { createOptimizedPicture } from '../../scripts/scripts.js';
+
 export default function decorate(block) {
-  block.innerHTML = `
-    <picture>
-      <source 
-        srcset="https://images.unsplash.com/photo-1503264116251-35a269479413?w=360&auto=format&fit=crop"
-        media="(max-width: 480px)"
-        type="image/jpeg">
-      <source 
-        srcset="https://images.unsplash.com/photo-1503264116251-35a269479413?w=768&auto=format&fit=crop"
-        media="(min-width: 481px) and (max-width: 1024px)"
-        type="image/jpeg">
-      <source 
-        srcset="https://images.unsplash.com/photo-1503264116251-35a269479413?w=1200&auto=format&fit=crop"
-        media="(min-width: 1025px)"
-        type="image/jpeg">
-      <img
-        src="https://images.unsplash.com/photo-1503264116251-35a269479413?w=360&auto=format&fit=crop"
-        alt=""
-        width="360"
-        height="240"
-        loading="eager"
-        decoding="async"
-      
-      />
-    </picture>
-    <div class="hero-text">
-      <h1>Welcome to My Portfolio</h1>
-      <p>This site was built with AEM Live to showcase how fast and efficient modern web tools can be.</p>
-      <a class="button" href="/contact">Contact Me</a>
-    </div>
+  block.textContent = ''; 
+
+  const picture = createOptimizedPicture(
+    'https://images.unsplash.com/photo-1503264116251-35a269479413', // src без ?params
+    'Abstract background',
+    true, // eager — т.к. LCP
+    [
+      { media: '(min-width: 1200px)', width: '1200' },
+      { media: '(min-width: 600px)', width: '800' },
+      { width: '480' }, 
+    ]
+  );
+
+  const wrapper = document.createElement('div');
+  wrapper.className = 'hero-text';
+  wrapper.innerHTML = `
+    <h1>Welcome to My Portfolio</h1>
+    <p>This site was built with AEM Live to showcase how fast and efficient modern web tools can be.</p>
+    <a class="button" href="/contact">Contact Me</a>
   `;
+
+  block.append(picture, wrapper);
 }
